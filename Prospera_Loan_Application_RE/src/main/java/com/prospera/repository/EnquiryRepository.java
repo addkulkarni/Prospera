47942d7b046a11f07991969306ea86c96b0e187e
pranali <pranalibhanage081@gmail.com>
2024-08-08 20:28:10 +0530

    In RE module forwardedforVerification Api added

--- Prospera_Loan_Application_RE/src/main/java/com/prospera/repository/EnquiryRepository.java
+++ Prospera_Loan_Application_RE/src/main/java/com/prospera/repository/EnquiryRepository.java
@@ -1,6 +1,9 @@
 package com.prospera.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 
 import com.prospera.model.Enquiry;
@@ -9,4 +12,10 @@ import com.prospera.model.Enquiry;
 public interface EnquiryRepository extends JpaRepository<Enquiry, Integer>
 {
 
+	List<Enquiry> findByEnquiryStatus(String string);
+
+	
+	
+
+	
 }
