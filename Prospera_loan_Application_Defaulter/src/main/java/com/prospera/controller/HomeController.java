2e06513848b807c9c9fbd7890c38cc75ed5f935e
Gayatrishinde02 <gayatrishinde176@gmail.com>
2024-08-14 16:11:00 +0530

    In defaulter module add restTemplate

--- Prospera_loan_Application_Defaulter/src/main/java/com/prospera/controller/HomeController.java
+++ Prospera_loan_Application_Defaulter/src/main/java/com/prospera/controller/HomeController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.prospera.model.Customer;
 import com.prospera.servicei.CustomerServiceI;
@@ -19,6 +21,9 @@ public class HomeController
   @Autowired
   CustomerServiceI csi;
   
+  @Autowired
+  RestTemplate restT;
+  
   @GetMapping("getalldefaulter")
 	public ResponseEntity<List<Customer>> getAllDefaulter()
 	{
@@ -26,4 +31,12 @@ public class HomeController
 		ResponseEntity<List<Customer>> response = new ResponseEntity<List<Customer>>(l,HttpStatus.OK);
 		return response;
 	}
+  
+//  @GetMapping("/getByEnquiryStatus/{enquiryStatus}")
+//  public List getByEnquiryStatus(@PathVariable("enquiryStatus") String enquiryStatus)
+//  {
+//	  String url="http://localhost:9097/getByEnquiryEnquiryStatus/"+enquiryStatus+"";
+//	  List al=restT.getForObject(url, List.class);
+//	  return al;
+//  }
 }
